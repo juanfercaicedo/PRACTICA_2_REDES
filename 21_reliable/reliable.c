09c01567fbcd31b28937de5c5cd5b71a5d18ec94
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-01-07 18:03:18 +0100

    .

--- 21_reliable/reliable.c
+++ 21_reliable/reliable.c
@@ -1,80 +1,79 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 #include "rlib.h"
 
-static struct { char data[MAX_PAYLOAD]; int len; } window_buffer[16];
-static uint32_t base_seq; 
-static uint32_t next_seq; 
-static uint32_t expected_seq;
-static int w_size;
-static long t_val;
+static char last_payload[MAX_PAYLOAD]; 
+static int last_payload_size;          
+static int waiting_for_ack;            
+static uint32_t next_seqno;            
+static uint32_t expected_seqno;        
+static long timeout_val;               
 
 void connection_initialization(int window_size, long timeout_in_ns) {
-    base_seq = 1; 
-    next_seq = 1; 
-    expected_seq = 1;
-    w_size = window_size; 
-    t_val = timeout_in_ns;
-    memset(window_buffer, 0, sizeof(window_buffer));
+    waiting_for_ack = 0;
+    next_seqno = 1;      
+    expected_seqno = 1; 
+    timeout_val = timeout_in_ns; 
+    last_payload_size = 0;
+
 }
 
 void receive_callback(packet_t *pkt, size_t pkt_size) {
-    // Corregido: Eliminado
-    if (VALIDATE_CHECKSUM(pkt) == 0) return; 
+    if (VALIDATE_CHECKSUM(pkt) == 0) {
+        return; 
+    }
 
     if (IS_ACK_PACKET(pkt)) {
-        if (pkt->ackno > base_seq) {
-            for (uint32_t i = base_seq; i < pkt->ackno; i++) {
-                CLEAR_TIMER(i % 16);
-            }
-            base_seq = pkt->ackno;
-            RESUME_TRANSMISSION(); 
+        if (waiting_for_ack && pkt->ackno > (next_seqno - 1)) {
+            CLEAR_TIMER(0);           
+            waiting_for_ack = 0;
+            RESUME_TRANSMISSION();    
+
+
         }
-    } else {
-        if (pkt->seqno == expected_seq) {
-            // Corregido: Eliminado
+    } 
+    else {
+        if (pkt->seqno == expected_seqno) {
             ACCEPT_DATA(pkt->data, pkt->len - DATA_PACKET_HEADER); 
-            expected_seq++;
+            expected_seqno++; 
         }
-        SEND_ACK_PACKET(expected_seq); 
+        
+        SEND_ACK_PACKET(expected_seqno);
     }
 }
 
 void send_callback() {
-    while (next_seq < base_seq + w_size) {
-        char buf[MAX_PAYLOAD];
-        // Corregido: Eliminado
-        int r = READ_DATA_FROM_APP_LAYER(buf, MAX_PAYLOAD); 
-        
-        if (r <= 0) break;
-
-        int idx = next_seq % 16;
-        memcpy(window_buffer[idx].data, buf, r);
-        window_buffer[idx].len = r;
-
-        // Corregido: Eliminado
-        SEND_DATA_PACKET(r + DATA_PACKET_HEADER, 0, next_seq, window_buffer[idx].data); 
-        // Corregido: Eliminado
-        SET_TIMER(idx, t_val); 
-        next_seq++;
+    if (waiting_for_ack) {
+        PAUSE_TRANSMISSION(); 
+        return;
     }
 
-    if (next_seq == base_seq + w_size) {
-        // Corregido: Eliminado
+
+    char buffer[MAX_PAYLOAD];
+    int bytes_read = READ_DATA_FROM_APP_LAYER(buffer, MAX_PAYLOAD);
+
+
+    if (bytes_read > 0) {
+        memcpy(last_payload, buffer, bytes_read);
+        last_payload_size = bytes_read;
+
+
+        SEND_DATA_PACKET(bytes_read + DATA_PACKET_HEADER, 0, next_seqno, last_payload);
+        
+        SET_TIMER(0, timeout_val); 
+        
+        next_seqno++;
+        waiting_for_ack = 1; 
         PAUSE_TRANSMISSION(); 
     }
 }
 
 void timer_callback(int timer_number) {
-    for (uint32_t i = base_seq; i < next_seq; i++) {
-        int idx = i % 16;
-        SEND_DATA_PACKET(window_buffer[idx].len + DATA_PACKET_HEADER, 0, i, window_buffer[idx].data);
-        SET_TIMER(idx, t_val); 
+    if (timer_number == 0 && waiting_for_ack) {
+        SEND_DATA_PACKET(last_payload_size + DATA_PACKET_HEADER, 0, next_seqno - 1, last_payload);
+        SET_TIMER(0, timeout_val); 
+
     }
 }
